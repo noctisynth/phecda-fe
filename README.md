5cd4c40294f0fd6704630bafa073b62864642c4a
MoreKindness <f2248472180@outlook.com>
2025-12-08 23:04:07 +0800

    init: Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Phecda-website
+
---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+// 路由配置
+import { createBrowserRouter } from 'react-router';
+import { Home } from './page/home';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    }
+])
+
