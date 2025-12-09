c8ee076f318c0dc25538b83b62ae8464559eb2ba
MoreKindness <f2248472180@outlook.com>
2025-12-09 23:07:07 +0800

    feat: add pre-commit

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 
 export const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Home</h1>
+      <span>fff</span>
+    </div>
+  );
+};
